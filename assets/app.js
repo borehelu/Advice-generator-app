c8381527e0bd559fd9af32ce75440b0bb50ac4b0
borehelu <borehelu@gmail.com>
2022-04-08 14:36:48 +0300

    code refactor for api call

--- assets/app.js
+++ assets/app.js
@@ -2,12 +2,18 @@ const quote_id = document.querySelector("h2 span");
 const quote = document.querySelector("q");
 const newQuote = document.querySelector(".new-quote");
 
+
+
 newQuote.addEventListener("click",function(){
     fetch('https://api.adviceslip.com/advice')
-    .then(data => data.json())
-    .then(data => {
-        let {id,advice} = data.slip;
-        quote.innerHTML = advice;
-        quote_id.innerHTML = id;
-    });
+    .then(response => {
+        response.json()
+        .then(data => {
+            let {id,advice} = data.slip;
+            quote.innerHTML = advice;
+            quote_id.innerHTML = id;
+
+        }).catch(err => console.log("Unable to fetch quote. Error ",err.message))
+    })
 });
+  
